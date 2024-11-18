15914fa0c6fdae845f4e44aa114578a4735d6b2f
MagicInUse <jacob.norr@live.com>
2024-11-18 01:59:20 -0700

    server side auth-routes taken care of.

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -4,7 +4,29 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
-  // TODO: If the user exists and the password is correct, return a JWT token
+  // TODONE: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ where: { username } });
+
+  if (!user) {
+    return res.status(401).json({ message: 'Invalid username or password' });
+  }
+
+  const passwordIsValid = await bcrypt.compare(password, user.password);
+  
+  if (!passwordIsValid) {
+    return res.status(401).json({ message: 'Invalid username or password' });
+  }
+
+  const secretKey = process.env.JWT_SECRET_KEY;
+
+  if (!secretKey) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+
+  const token = jwt.sign({ id: user.id, username: user.username }, secretKey, { expiresIn: '5m' });
+  return res.json({ token });
 };
 
 const router = Router();
