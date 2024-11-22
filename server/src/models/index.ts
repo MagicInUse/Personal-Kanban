274ba83622c998dbcb53575f7d50b54f50167343
MagicInUse <jacob.norr@live.com>
2024-11-21 20:27:09 -0700

    sequelize setup for non-local host

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -13,7 +13,7 @@ if (!process.env.DB_NAME || !process.env.DB_USER || !process.env.DB_PASSWORD || 
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
+      host: process.env.DB_URL || 'localhost',
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
