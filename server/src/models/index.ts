2322466091e1fe51a9c726b07d308fe2e4fb2aff
MagicInUse <jacob.norr@live.com>
2024-11-21 20:33:32 -0700

    rename

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -10,10 +10,10 @@ if (!process.env.DB_NAME || !process.env.DB_USER || !process.env.DB_PASSWORD || 
   throw new Error('Database configuration is missing');
 }
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+const sequelize = process.env.DB_HOST
+  ? new Sequelize(process.env.DB_HOST)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: process.env.DB_URL || 'localhost',
+      host: process.env.DB_HOST || 'localhost',
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
